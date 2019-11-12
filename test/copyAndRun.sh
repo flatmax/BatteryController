3e29f6ed8012a06e356092f1def28d7009261351
Matt Flax <flatmax@flatmax.org>
2019-11-13 08:29:30 +1100

    code cleanup

--- test/copyAndRun.sh
+++
@@ -1,4 +0,0 @@
-host=192.168.1.45
-scp -r ../../BatteryController root@$host:
-# ssh root@$host "killall pigpiod; rm /var/run/pigpio.pid; node ./test.js"
-ssh root@$host "cd BatteryController/test; node ./test.js"
