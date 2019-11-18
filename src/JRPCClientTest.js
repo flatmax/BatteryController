f7f18d0879cf0a687513ecb4ead01a87f8e78d5a
Matt Flax <flatmax@flatmax.org>
2019-11-18 14:27:12 +1100

    Using the BSD clause 3 license

--- src/JRPCClientTest.js
+++ src/JRPCClientTest.js
@@ -1,24 +1,57 @@
+/**
+Copyright (c) 2019 The Battery Controller Authors. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+
+    * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+ copyright notice, this list of conditions and the following disclaimer
+ in the documentation and/or other materials provided with the
+ distribution.
+    * Neither the name of Flatmax Pty. Ltd. nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 "use strict";
 
 const JRPC = require('./JRPC').JRPC;
 
 const jrpc = new JRPC;
 jrpc.findAvailableServers();
-return
-const host = "192.168.1.183"
-jrpc.connectToServer({host:host, port:9001});
-let reqs = [jrpc.getBCCnt(),
-            jrpc.getUICnt()];
+// return
+// const host = "192.168.1.183"
+// jrpc.connectToServer({host:host, port:9001});
+setInterval(()=>{
+    let reqs = [jrpc.getBCCnt(),
+              jrpc.getUICnt()];
 
-Promise.all(reqs).then((responses)=>{
-  console.log('nB '+responses[0].result);
-  console.log('nU '+responses[1].result);
-});
-
-reqs = [jrpc.turnOnBC(1),
-            jrpc.turnOnUI(2)];
-
-Promise.all(reqs).then((responses)=>{
-  console.log('nB '+responses[0].result);
-  console.log('nU '+responses[1].result);
-});
+    Promise.all(reqs).then((responses)=>{
+      console.log('nB '+responses[0].result);
+      console.log('nU '+responses[1].result);
+    })
+  }
+  , 1000
+);
+// return
+// reqs = [jrpc.turnOnBC(1),
+//             jrpc.turnOnUI(2)];
+//
+// Promise.all(reqs).then((responses)=>{
+//   console.log('nB '+responses[0].result);
+//   console.log('nU '+responses[1].result);
+// });
