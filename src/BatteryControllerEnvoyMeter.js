12115030aa449d3723ea4154470351ad3e6fb4a9
Matt Flax <flatmax@flatmax.org>
2020-01-02 21:34:30 +1100

    Altered license to define "Source Code"

--- src/BatteryControllerEnvoyMeter.js
+++ src/BatteryControllerEnvoyMeter.js
@@ -1,11 +1,14 @@
 /**
 Copyright (c) 2019 The Battery Controller Authors. All rights reserved.
 
+ This licence relates to source code for the Battery Controller outined below ("Source Code").
+
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are
  met:
 
-    * Redistributions of source code must retain the above copyright
+    * Redistributions of Source Code must retain the above copyright
  notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above
  copyright notice, this list of conditions and the following disclaimer
@@ -15,7 +18,7 @@ Copyright (c) 2019 The Battery Controller Authors. All rights reserved.
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ THIS Source Code IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
@@ -25,7 +28,7 @@ Copyright (c) 2019 The Battery Controller Authors. All rights reserved.
  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ OF THIS Source Code, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 'use strict';
 const http = require('http');
